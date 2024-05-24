5586437bc15d9a2a7a5e97bfb85de9d0255d9de8
rizwanc018 <rizwanc018@gmail.com>
2024-05-24 10:16:49 +0300

    Env variable created to store url for trpc provider

--- src/app/_trpc/Providers.tsx
+++ src/app/_trpc/Providers.tsx
@@ -5,13 +5,18 @@ import { httpBatchLink } from "@trpc/client"
 import React, { useState } from "react"
 import { trpc } from "./client"
 
+const apiURL = process.env.NEXT_PUBLIC_API_URL as string
+
+if (!apiURL) {
+    throw new Error("API URL is not defined in the environment variables")
+}
 export default function Provider({ children }: { children: React.ReactNode }) {
     const [queryClient] = useState(() => new QueryClient())
     const [trpcClient] = useState(() =>
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: "http://localhost:3000/api/trpc",
+                    url: apiURL as string,
                 }),
             ],
         })
@@ -23,3 +28,29 @@ export default function Provider({ children }: { children: React.ReactNode }) {
         </trpc.Provider>
     )
 }
+
+// "use client"
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+// import { httpBatchLink } from "@trpc/client"
+// import React, { useState } from "react"
+// import { trpc } from "./client"
+
+// export default function Provider({ children }: { children: React.ReactNode }) {
+//     const [queryClient] = useState(() => new QueryClient())
+//     const [trpcClient] = useState(() =>
+//         trpc.createClient({
+//             links: [
+//                 httpBatchLink({
+//                     url: "http://localhost:3000/api/trpc",
+//                 }),
+//             ],
+//         })
+//     )
+
+//     return (
+//         <trpc.Provider client={trpcClient} queryClient={queryClient}>
+//             <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+//         </trpc.Provider>
+//     )
+// }
