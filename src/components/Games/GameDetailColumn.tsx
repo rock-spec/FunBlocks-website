54a25170436724ed3cdc984e7d5eed97dcb0f160
funblocksdev <162561352+funblocksdev@users.noreply.github.com>
2024-08-30 18:13:40 +0800

    Bug fix 3 (#100)
    * new bugs fix
    
    * new bullet point and event in news and article fix

--- src/components/Games/GameDetailColumn.tsx
+++ src/components/Games/GameDetailColumn.tsx
@@ -139,7 +139,7 @@ export const GameDetailColumn = async ({ data, locale }: { data: any; locale: Lo
                                 imageUrl: `${event.pic}?height=360&width=720`,
                                 title: event?.[`title_${locale}`] || event.title_en,
                                 details: `${formatDate(event?.startdate)} - ${formatDate(event.enddate)}`,
-                                timezone: event?.timezone,
+                                timezone: event?.timezone?.split(":")[0],
                                 tags: [event?.game?.gameid],
                                 onFirstButtonClick: () => {},
                                 onSecondButtonClick: () => {},
